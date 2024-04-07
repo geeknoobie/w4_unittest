ae96ae94661acccb927969ce702902de676d0fea
geeknoobie <70904910+geeknoobie@users.noreply.github.com>
2024-04-06 22:24:39 -0400

    Update w4_unittest.py

--- w4_unittest.py
+++ w4_unittest.py
@@ -1948,16 +1948,16 @@ def test_gradient_descent(target, data, word2Ind, N, V, num_iters):
             },
             "expected": {
                 "W1": pickle.load(
-                    open("./support_files/gradient_descent/w1.pkl", "rb")
+                    open("/content/w4_unittest/w1.pkl", "rb")
                 ),
                 "W2": pickle.load(
-                    open("./support_files/gradient_descent/w2.pkl", "rb")
+                    open("/content/w4_unittest/w2.pkl", "rb")
                 ),
                 "b1": pickle.load(
-                    open("./support_files/gradient_descent/b1.pkl", "rb")
+                    open("/content/w4_unittest/b1.pkl", "rb")
                 ),
                 "b2": pickle.load(
-                    open("./support_files/gradient_descent/b2.pkl", "rb")
+                    open("/content/w4_unittest/b2.pkl", "rb")
                 ),
             },
         },
